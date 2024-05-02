e2c0889aea5654ef91904e62114a4bd21db77829
n-takeshita <naokisyuu@gmail.com>
2024-05-02 15:19:44 +0900

    add: shadcn-ui
    https://ui.shadcn.com/docs/installation/vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react-swc'
@@ -5,4 +6,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
