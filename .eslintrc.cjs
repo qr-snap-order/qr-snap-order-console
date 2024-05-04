a0b772d4faf14d1d27421b9c161a644d00fa82a5
n-takeshita <naokisyuu@gmail.com>
2024-05-04 20:19:45 +0900

    add: plugin:react/recommended of eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,14 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    /**
+     * If you are using the new JSX transform from React 17, extend react/jsx-runtime in your
+     * eslint config (add "plugin:react/jsx-runtime" to "extends") to disable the relevant rules.
+     *
+     * @see https://www.npmjs.com/package/eslint-plugin-react/v/7.31.6
+     */
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
@@ -14,5 +22,9 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    /**
+     * @see https://github.com/shadcn-ui/ui/issues/120#issuecomment-1742144083
+     */
+    'react/prop-types': [2, { ignore: ['className'] }],
   },
 }
