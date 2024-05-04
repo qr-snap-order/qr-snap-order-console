49d167e40f13ac89289acd9cb327945843db1057
n-takeshita <naokisyuu@gmail.com>
2024-05-04 20:20:36 +0900

    add: eslint-config-prettier of eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
      */
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
