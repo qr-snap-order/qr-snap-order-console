d2b21c6da4b4a80672021fcd787e0f31bcfd534d
n-takeshita <naokisyuu@gmail.com>
2024-05-05 19:53:16 +0900

    add: bunx --bun shadcn-ui@latest add sonner

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 
+import { Toaster } from '@/components/ui/sonner'
 import '@/index.css'
 import { client } from '@/lib/apolloClient'
 import { router } from '@/lib/router'
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <RouterProvider router={router} />
+      <Toaster />
     </ApolloProvider>
   </React.StrictMode>
 )
