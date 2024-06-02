b88b8e8eca3c300a64abc0caf07a109d70716367
n-takeshita <naokisyuu@gmail.com>
2024-06-03 02:59:43 +0900

    add: sidenav

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 
-import './App.css'
+import { SideNav } from './components/sidenav'
 
 function App() {
   return (
     <>
-      {/* TODO:: Spinner Componentが実装されたら置き換える
-       * @see https://github.com/shadcn-ui/ui/pull/3554
-       */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <Outlet />
-      </Suspense>
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-64 lg:flex-col">
+        <SideNav />
+      </div>
+      <div className="lg:pl-64">
+        <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+          <header>Header</header>
+        </div>
+        <main className="p-4 sm:px-6 lg:px-8">
+          {/* TODO:: Spinner Componentが実装されたら置き換える
+           *
+           * @see https://github.com/shadcn-ui/ui/pull/3554
+           */}
+          <Suspense fallback={<div>Loading...</div>}>
+            <Outlet />
+          </Suspense>
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/components/sidenav.tsx
@@ -0,0 +1,57 @@
+import { Home, Mountain, Store, User, Users } from 'lucide-react'
+import { Link } from 'react-router-dom'
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/TTnTF2FR10A
+ * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
+ */
+
+const TenantIcon = Mountain
+
+const navItems = [
+  {
+    name: 'Home',
+    icon: Home,
+    to: '/',
+  },
+  {
+    name: 'Users',
+    icon: User,
+    to: '/users',
+  },
+  {
+    name: 'Shops',
+    icon: Store,
+    to: '/shops',
+  },
+  {
+    name: 'Employees',
+    icon: Users,
+    to: '/employees',
+  },
+]
+
+export function SideNav() {
+  return (
+    <nav className="h-full w-full bg-primary">
+      <div className="flex h-16 shrink-0 items-center justify-between px-4">
+        <Link className="flex items-center gap-2 text-gray-300" to="/">
+          <TenantIcon className="h-5 w-5" />
+          <span className="text-lg font-semibold text-gray-50">Tenant Inc</span>
+        </Link>
+      </div>
+      <div className="space-y-2 px-4 py-6">
+        {navItems.map((item) => (
+          <Link
+            key={item.name}
+            className="flex items-center gap-3 rounded-md px-3 py-2 text-sm font-medium text-gray-300 transition-all hover:bg-primary/80 hover:text-gray-50 dark:text-gray-400 dark:hover:bg-primary/80 dark:hover:text-gray-50"
+            to={item.to}
+          >
+            <item.icon className="h-5 w-5" />
+            {item.name}
+          </Link>
+        ))}
+      </div>
+    </nav>
+  )
+}
