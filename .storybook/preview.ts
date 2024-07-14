43cdc80b502ddd17dc5e4bfbd978c2983f2b3e81
n-takeshita <naokisyuu@gmail.com>
2024-07-15 00:24:10 +0900

    add: storybook-addon-remix-react-router for use Link component in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,9 @@
 import type { Preview } from '@storybook/react'
 import '../src/index.css'
+import { withRouter } from 'storybook-addon-remix-react-router'
 
 const preview: Preview = {
+  decorators: [withRouter],
   parameters: {
     controls: {
       matchers: {
