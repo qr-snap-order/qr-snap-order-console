4770222e2ce34120ab1f4b11d247157ed1a6c130
n-takeshita <naokisyuu@gmail.com>
2024-08-04 14:59:25 +0900

    fix: multiple-select design

--- src/components/organisms/multiple-select.tsx
+++ src/components/organisms/multiple-select.tsx
@@ -71,9 +71,11 @@ export function MultipleSelect({
   if (readOnly) {
     return (
       <div className="flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1">
-        {value.map((selected) => (
-          <Badge key={selected.id}>{selected.name}</Badge>
-        ))}
+        <div className="flex flex-wrap gap-2">
+          {value.map((selected) => (
+            <Badge key={selected.id}>{selected.name}</Badge>
+          ))}
+        </div>
       </div>
     )
   }
